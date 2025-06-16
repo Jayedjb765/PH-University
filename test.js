d0bf413a86776162d20559c688745722add7ae3c
Jayedjb765 <jayedjb76@gmail.com>
2025-06-16 21:35:35 +0600

    handeling  admin Sidebar  using progmatically way  for Ph-university

--- test.js
+++ test.js
@@ -25,23 +25,47 @@ const adminPaths2 = [
         ],
     },
 ];
-
 const newArray = adminPaths2.reduce((acc, iteam) => {
-    if (iteam.path && iteam.element) {
+    if (iteam.path && iteam.name) {
         acc.push({
-            path: iteam.path,
-            element: iteam.element,
+            key: iteam.path,
+            label: "This sholud be the label",
         })
 
     }
     if (iteam.children) {
-        iteam.children.forEach(child => {
-            acc.push({
-                path: child.path,
-                element: child.element
-            })
+        acc.push({
+            key: iteam.name,
+            label: iteam.name,
+            children: [
+                iteam.children.map(child => ({
+                    key: child.name,
+                    label: "navlink",
+
+
+                }))
+            ]
         })
     }
     return acc
 }, [])
-console.log(newArray)
+
+// const newArray = adminPaths2.reduce((acc, iteam) => {
+//     if (iteam.path && iteam.element) {
+//         acc.push({
+//             path: iteam.path,
+//             element: iteam.element,
+//         })
+
+//     }
+//     if (iteam.children) {
+//         iteam.children.forEach(child => {
+//             acc.push({
+//                 path: child.path,
+//                 element: child.element
+//             })
+//         })
+//     }
+//     return acc
+// }, [])
+console.log(JSON.stringify(newArray))
