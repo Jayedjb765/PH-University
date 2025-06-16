bcf608efca745131a353b5e157528cd7d9fcd130
Jayedjb765 <jayedjb76@gmail.com>
2025-06-17 00:58:10 +0600

    adding sidebar dynamically  for Ph-university

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,37 +1,54 @@
 import { Layout, Menu } from "antd";
 import { sidebarGenerator } from "../../utils/siderbargenerator";
 import { adminPaths } from "../../routes/admin.routes";
+import { facultyPaths } from "../../routes/faculty.route";
+import { studentPaths } from "../../routes/student.route";
 
 const { Sider } = Layout;
+
+const userRole = {
+  ADMIN: "admin",
+  FACULTY: "faculty",
+  STUDENT: "student",
+};
+
 const Sidebar = () => {
+  const role = "student";
+  let sidebarItems;
+
+  switch (role) {
+    case userRole.ADMIN:
+      sidebarItems = sidebarGenerator(adminPaths, userRole.ADMIN);
+      break;
+    case userRole.FACULTY:
+      sidebarItems = sidebarGenerator(facultyPaths, userRole.FACULTY);
+      break;
+    case userRole.STUDENT:
+      sidebarItems = sidebarGenerator(studentPaths, userRole.STUDENT);
+      break;
+
+    default:
+      break;
+  }
+
   return (
-    <Sider
-      breakpoint="lg"
-      collapsedWidth="0"
-      onBreakpoint={(broken) => {
-        console.log(broken);
-      }}
-      onCollapse={(collapsed, type) => {
-        console.log(collapsed, type);
-      }}
-    >
+    <Sider breakpoint="lg" collapsedWidth="0">
       <div
         style={{
           color: "white",
-          textAlign: "center",
           height: "4rem",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <h1>PH Uni </h1>
+        <h1>PH Uni</h1>
       </div>
       <Menu
         theme="dark"
         mode="inline"
         defaultSelectedKeys={["4"]}
-        items={sidebarGenerator(adminPaths, "student")}
+        items={sidebarItems}
       />
     </Sider>
   );
