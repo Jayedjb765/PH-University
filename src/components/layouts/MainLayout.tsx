646705164d570a312d26296b813111c262992097
Jayedjb765 <jayedjb76@gmail.com>
2025-06-16 12:39:30 +0600

    handeling  router for Ph-university

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,4 +1,5 @@
 import { Layout, Menu, type MenuProps } from "antd";
+import { Outlet } from "react-router-dom";
 
 const { Header, Content, Footer, Sider } = Layout;
 const items: MenuProps["items"] = [
@@ -71,7 +72,7 @@ const MainLayout = () => {
               minHeight: 360,
             }}
           >
-            <h1>The main content should go there</h1>
+            <Outlet />
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
