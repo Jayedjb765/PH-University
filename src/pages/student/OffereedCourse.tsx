03ead4d987b4e7ad451b16f531c77fa0c8b6338e
Jayedjb765 <jayedjb76@gmail.com>
2025-06-17 00:04:01 +0600

    adding student and faculty pages  for Ph-university

---
+++ src/pages/student/OffereedCourse.tsx
@@ -0,0 +1,9 @@
+const OffereedCourse = () => {
+  return (
+    <div>
+      <h1>OffereedCourse</h1>
+    </div>
+  );
+};
+
+export default OffereedCourse;
---
+++ src/pages/student/Studentashboard.tsx
@@ -0,0 +1,9 @@
+const Studentashboard = () => {
+  return (
+    <div>
+      <h2>Studentashboard</h2>
+    </div>
+  );
+};
+
+export default Studentashboard;
