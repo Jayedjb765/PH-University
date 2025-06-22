4eb41766c857459c7940267bac2b5111e416a700
Jayedjb765 <jayedjb76@gmail.com>
2025-06-22 22:41:00 +0600

    hadenling academic semester  for Ph-university

--- src/redux/api/basApi.ts
+++ src/redux/api/basApi.ts
@@ -1,12 +1,19 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { createApi } from "@reduxjs/toolkit/query/react";
-
+import type { RootState } from "../store";
+const basequery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/api/v1",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+    return headers;
+  },
+});
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
-    credentials: "include",
-  }),
-
+  baseQuery: basequery,
   endpoints: () => ({}),
 });
