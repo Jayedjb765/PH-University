015132579a092e36afb126dc4bbf93313f3795fe
Jayedjb765 <jayedjb76@gmail.com>
2025-06-24 23:50:40 +0600

    hadenling academic semester jwt  for Ph-university

--- src/redux/api/basApi.ts
+++ src/redux/api/basApi.ts
@@ -1,6 +1,7 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { createApi } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "../store";
+import { setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -17,9 +18,24 @@ const baseQuery = fetchBaseQuery({
 
 const baseQureyWithRefreshToken = async (args, api, extraOptions) => {
   const result = await baseQuery(args, api, extraOptions);
-  if (result.error?.status === 401) {
-    console.log("Sending refresh token");
+  if (result?.error?.status === 401) {
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+    const data = await res.json();
+    console.log(data);
+
+    const user = (api.getState() as RootState).auth.user;
+
+    api.dispatch(
+      setUser({
+        user,
+        token: data.data.accessToken,
+      })
+    );
   }
+  return result;
 };
 
 export const baseApi = createApi({
